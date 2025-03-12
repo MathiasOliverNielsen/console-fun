1e0ff12f3e98dda1c464c64c626570eac2eb2929
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-03-12 12:07:28 +0100

    Console logs

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,13 +1,25 @@
 // write cool JS here!!
+console.time('default');
 const myInfo = 'en variabel';
-const myBooks = ["The Catcher in the Rye",
-    "To Kill a Mockingbird",
-    "1984",
-    "Pride and Prejudice",
-    "The Great Gatsby",
-    "The Hobbit",
-    "Harry Potter and the Sorcerer's Stone",
-    "The Lord of the Rings",
-    "The Da Vinci Code",
-    "The Hunger Games"]
+const myBooks = [
+  'The Catcher in the Rye',
+  'To Kill a Mockingbird',
+  '1984',
+  'Pride and Prejudice',
+  'The Great Gatsby',
+  'The Hobbit',
+  "Harry Potter and the Sorcerer's Stone",
+  'The Lord of the Rings',
+  'The Da Vinci Code',
+  'The Hunger Games',
+];
 
+console.group('konsol opgave');
+console.log('Jeg er en almindelig konsol log');
+console.info('Jeg er en  konsol info: ' + myInfo);
+console.warn('Jeg er en advarsel');
+console.error('Jeg er en fejl');
+
+console.table(myBooks);
+console.groupEnd();
+console.timeEnd('default');
